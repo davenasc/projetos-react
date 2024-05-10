f12030e71161b0ea5bc2492395edd5d810ff6f2f
David Nascimento <powdx@live.com.pt>
2024-05-10 16:32:21 -0300

    first up

---
+++ calculadora/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+/*ReactDOM.render(
+  <div>
+    <h1>calculadora</h1>
+    <Calculator />
+  </div>,
+    document.getElementById('root'));
+reportWebVitals();*/
+
+ReactDOM.render(
+  <div>
+
+  <Calculator />, document.getElementById('root')
+  </div>
+);
+reportWebVitals();
